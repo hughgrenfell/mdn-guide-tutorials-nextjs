6116f0e8cc0c67edb72c728263f7d01c9272922d
hughgrenfell <jwright@grenfellproject.ca>
2023-09-23 10:10:03 -0500

    test remote after changing repo name

--- src/app/projects/animation-study/page.tsx
+++ src/app/projects/animation-study/page.tsx
@@ -1,5 +1,16 @@
 export default function AnimationStudy() {
 
+    const aliceTumbling = [
+        { transform: 'rotate(0) scale(1)' },
+        { transform: 'rotate(720deg) scale(0)' }
+    ];
+
+    const aliceTiming = {
+        duration: 2000,
+        iterations: 1,
+        fill: 'forwards'
+    }
+
     return (
         <>
             <div>This is where the animation study will live</div>
