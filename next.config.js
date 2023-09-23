4bd88a993f092442940bd4c33bacf7b5cb07fa5b
hughgrenfell <jwright@grenfellproject.ca>
2023-09-23 09:40:47 -0500

    fix output: "export" error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /*
-    // Remove this comment and surroned comments tags when ready to deploy!
     output: "export",
-    */
 }
 
 module.exports = nextConfig
