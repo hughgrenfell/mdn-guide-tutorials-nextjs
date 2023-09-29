7fcc688f373c243ae522874fb977e24c399339ca
hughgrenfell <jwright@grenfellproject.ca>
2023-09-29 13:25:37 -0500

    change config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            output: "export",
-            images: { unoptimized: true },
-        }
-    }
-
-    return {
-        images: { unoptimized: true },
-    }
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+  }
+   
+  module.exports = nextConfig
